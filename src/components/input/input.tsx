1cff52d3273b0cf93e2b98aeaecff5451125485c
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-25 13:58:37 +0330

    create secure useastate and add type for input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,15 +1,16 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import IInputProps from "./input.types";
 import styles from './input.module.scss'
 
 
 const Input = (props: IInputProps) => {
+    const [secure, setSecure] = useState<boolean>(props.type === "password" ? true : false)
     return (
         <div className={styles.input}>
             {props.label && <span className={styles.label}>{props.label}</span>}
             <div className={styles.input_container}>
                 {props.icon && props.icon}
-                <input {...props} />
+                <input {...props} type={secure ? "password" : "text"}/>
             </div>
             {props.error && <span className={styles.error}>{props.error}</span>}
         </div>
