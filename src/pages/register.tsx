629a10e412daa36ba7cd26446630ef424b76f1bd
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-25 12:01:45 +0330

    add icon for inputs

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -13,8 +13,18 @@ const Register = () => {
             <form className="form">
                 <span>Welcome Back</span>
                 <span>Enter your credentials to access your account</span>
-                <Input></Input>
-                <Input></Input>
+                <Input 
+                   icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                   <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                 </svg>
+                 } 
+                />
+                <Input 
+                        icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" >
+                        <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                      </svg>
+                      }
+                />
                 <Button onClick={() => { }}>
                     <span>Sginin</span>
                 </Button>
