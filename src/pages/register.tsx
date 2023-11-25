b6101609b1bdaa5627a068d6df7e30379839b25b
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-11-25 11:35:59 +0330

    format document

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,22 +1,21 @@
 import Button from "../components/button/button"
 import Input from "../components/input/input"
 
-const Register = () =>{
+const Register = () => {
     return (
         <div style={{
-            display : "flex",
-            flex : 1,
-            flexDirection : "column",
-            justifyContent : "center",
-            alignItems : "center"
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
         }}>
             <form className="form">
                 <span>Welcome Back</span>
                 <span>Enter your credentials to access your account</span>
                 <Input></Input>
                 <Input></Input>
-
-                <Button onClick={() => {}}>
+                <Button onClick={() => { }}>
                     <span>Sginin</span>
                 </Button>
             </form>
