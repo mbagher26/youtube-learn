e47cc7b481d8b6b91b828720c27969d2c2313e31
mbagher26 <mohammadbaghermollanory@gmail.com>
2023-12-10 09:29:54 +0330

    update home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,28 @@
-import { useState , useMemo, useRef} from "react"
-
+import { useState , useEffect} from "react"
+import { useNavigate } from "react-router-dom"
 const Home = () => {
-    const [birthdate, setBirthdate] = useState<number>()
-    const myInput = useRef<any>()
-
-    const handelBirthdateSubmit = () =>{
-        console.log("setBirthdate")
-        setBirthdate(myInput.current.value)
-    }
-
-    const calculator = useMemo(() => {
-        console.log("calculation hapend")
-        const currentYear = new Date().getFullYear()
-        if(birthdate){
-            return currentYear - birthdate
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        name : ""
+    })
+    useEffect(() =>{
+        const user =localStorage.getItem('user')
+        if(user){
+            setUser(JSON.parse(user))
+        }else{
+            navigate("/login")
         }
-    },[birthdate])
+    }, [])
+
+ 
     
     console.log("rerender")
     return(
         <>
-            <h2>Age calculator</h2>
-            <input ref={myInput} placeholder="enter your birthdate"/>
-            <button onClick={handelBirthdateSubmit}>submit</button>
-            <br/>
-            your age is {calculator}
+            {
+                user?.name &&
+                <span>Welcome {user.name}</span>
+            }
         </>
     )
 }
